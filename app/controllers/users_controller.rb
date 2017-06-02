8177cb9e82c81e13e74227b99fce8aaba2e47458
Andrew Kim <andrew@Andrews-MacBook-Pro.local>
2017-06-01 21:18:10 -0700

    work

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,6 @@
 class UsersController < ApplicationController
-  def new
-  end
-
-  def create
-  end
 
   def show
+    @user = User.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   resources :games
 
   root 'welcome#index'
+  resources :users, only: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
